ecc6d18dd229d2d8cbd3ff343f38b31d6b04445b
Šarūnas Navickas <zaibacu@gmail.com>
2020-07-31 14:55:06 +0300

    Remove unused stuff

--- src/test/java/zaibacu/graphql/providers/UtilsTest.java
+++ src/test/java/zaibacu/graphql/providers/UtilsTest.java
@@ -1,23 +1,14 @@
 package zaibacu.graphql.providers;
 
 import org.junit.Test;
-import zaibacu.graphql.services.HttpService;
 
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 
 public class UtilsTest {
     @Test
     public void testParseResult() throws IOException {
-        HttpService service = mock(HttpService.class);
-
-        QueryProvider provider = new QueryProvider()
-                .withName("test")
-                .withParameter("id", "123")
-                .withParameter("number", 321);
-
         String json = "{\"results\": [{\"name\": \"test1\", \"age\": 1, \"inner\": {\"other\": \"test\"}}," +
                         "{\"name\": \"test2\", \"age\": 2, \"inner\": {\"other\": \"test\"}}]}";
 
